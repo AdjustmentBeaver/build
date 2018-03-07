320973016aaf7a0471672e7e2f9c35fe89eaf5cc
Istvan Telek <telek96+github@gmail.com>
2018-03-07 19:26:34 +0100

    Fixed new targets

--- rpi3.mk
+++ rpi3.mk
@@ -115,16 +115,19 @@ $(RPI3_UBOOT_ENV): $(RPI3_UBOOT_ENV_TXT) u-boot
 u-boot-env-clean:
 	rm -f $(RPI3_UBOOT_ENV)
 
-gen-pubkey: mkdir $(ROOT)/out/keys && cd $(ROOT)/out/keys
+gen-pubkey:
+	mkdir $(ROOT)/out/keys && cd $(ROOT)/out/keys
 	openssl genrsa -F4 -out dev.key 2048
 	openssl req -batch -new -x509 -key dev.key -out dev.crt
 
-u-boot-fit: mkdir $(ROOT)/out/fit && cd $(ROOT)/out/fit
+u-boot-fit:
+	mkdir $(ROOT)/out/fit && cd $(ROOT)/out/fit
 	ln -s $(LINUX_IMAGE) && ln -s $(ARM_TF_BOOT) && ln -s $(LINUX_DTB)
 	cp $(LINUX_DTB) rpi3_pubkey.dtb
 	$(U-BOOT_PATH)/tools/mkimage -f $(ROOT)/out/rpi3_fit.its -K rpi3_pubkey.dtb -k keys -r image.fit
 
-u-boot-fit-clean: rm -rf $(ROOT)/out/fit
+u-boot-fit-clean:
+	rm -rf $(ROOT)/out/fit
 
 ################################################################################
 # Busybox
