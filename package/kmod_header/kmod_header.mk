3662f44f6c6f902a8d92c11bab71b7d909793581
Istvan Telek <telek96+github@gmail.com>
2018-06-26 17:00:19 +0200

    Updates

--- package/kmod_header/kmod_header.mk
+++ package/kmod_header/kmod_header.mk
@@ -3,7 +3,7 @@ KMOD_HEADER_SITE = $(BR2_PACKAGE_KMOD_HEADER_SITE)
 KMOD_HEADER_SITE_METHOD = local
 
 define KMOD_HEADER_BUILD_CMDS
-        $(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) \
+	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) \
 	ARCH=$(KERNEL_ARCH) \
 	CROSS_COMPILE=$(TARGET_CROSS) \
 	LINUX_DIR=$(LINUX_DIR)
@@ -11,6 +11,7 @@ endef
 
 define KMOD_HEADER_POST_BUILD_EXDBG
 	python3 $(@D)/dwarfparse/dwarfparse.py -o $(@D) $(@D)/kmod_header.ko
+	mkdir -p $(STAGING_DIR)/ta_dev_kit/include
 	cp $(@D)/cu_0.h $(STAGING_DIR)/ta_dev_kit/include/kmod_header.h
 endef
 
